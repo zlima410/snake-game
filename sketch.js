9baf8058b2d37df67b1a38317c195163d4ea1a97
zlima410 <endercraft68@gmail.com>
2023-12-07 01:09:43 -0500

    Polished the game mechanics and added an end result

--- sketch.js
+++ sketch.js
@@ -1,48 +1,52 @@
-var s;
-var scl = 20;
-
-var food;
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
 
 function setup() {
     createCanvas(600, 600);
-    s = new Snake();
+    w = floor(width / rez);
+    h = floor(height / rez);
+    snake = new Snake();
     frameRate(10);
-    pickLocation();
+    foodLocation();
 }
 
-function pickLocation() {
-    var cols = floor(width/scl);
-    var rows = floor(height/scl);
-    food = createVector(floor(random(cols)), floor(random(rows)));
-    food.mult(scl);
-}
-
-function mosuePressed() {
-    s.total++;
-}
-
-function draw() {
-    background(51);
-
-    if (s.eat(food)) {
-        pickLocation();
-    }
-    s.death();
-    s.update();
-    s.show();
-
-    fill(255, 0, 100);
-    rect(food.x, food.y, scl, scl);
+function foodLocation() {
+    let x = floor(random(w));
+    let y = floor(random(h));
+    food = createVector(x, y);
 }
 
 function keyPressed() {
     if (keyCode === UP_ARROW) {
-        s.dir(0, -1);
+        snake.setDir(0, -1);
     } else if (keyCode === DOWN_ARROW) {
-        s.dir(0, 1);
+        snake.setDir(0, 1);
     } else if (keyCode === RIGHT_ARROW) {
-        s.dir(1, 0);
+        snake.setDir(1, 0);
     } else if (keyCode === LEFT_ARROW) {
-        s.dir(-1, 0);
+        snake.setDir(-1, 0);
     }
+}
+
+function draw() {
+    scale(rez);
+    background(220);
+    if (snake.eat(food)) {
+        foodLocation();
+    }
+    snake.update();
+    snake.show();
+
+    if (snake.endGame()) {
+        print("END GAME");
+        background(255, 0, 0);
+        noLoop();
+      }
+
+    noStroke();
+    fill(255, 0, 0);
+    rect(food.x, food.y, 1, 1);
 }
--- snake.js
+++ snake.js
@@ -1,59 +1,61 @@
-function Snake() {
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = 1;
-    this.yspeed = 0;
-    this.total = 0;
-    this.tail = [];
+class Snake {
+  constructor() {
+    this.body = [];
+    this.body[0] = createVector(floor(w/2), floor(h/2));
+    this.xdir = 0;
+    this.ydir = 0;
+    this.len = 0;
+  }
 
-    this.eat = function(pos) {
-        var d = dist(this.x, this.y, pos.x, pos.y);
-        if (d < 1) {
-            this.total++;
-            return true;
-        } else {
-            return false;
-        }
+  setDir(x, y) {
+    this.xdir = x;
+    this.ydir = y;
+  }
+
+  update() {
+    let head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+
+  grow() {
+    let head = this.body[this.body.length - 1].copy();
+    this.len++;
+    this.body.push(head);
+  }
+
+  endGame() {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+      return true;
     }
-
-    this.dir = function(x, y) {
-        this.xspeed = x;
-        this.yspeed = y;
+    for (let i = 0; i < this.body.length - 1; i++) {
+      let part = this.body[i];
+      if (part.x == x && part.y == y) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    this.death = function() {
-        for (var i = 0; i < this.tail.length; i++) {
-            var pos = this.tail[i];
-            var d = dist(this.x, this.y, pos.x, pos.y);
-            if (d < 1) {
-                console.log('starting over');
-                this.total = 0;
-                this.tail = [];
-            }
-        }
+  eat(pos) {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x == pos.x && y == pos.y) {
+      this.grow();
+      return true;
     }
+    return false
+  }
 
-    this.update = function() {
-        if (this.total === this.tail.length) {
-            for (var i = 0; i < this.tail.length-1; i++) {
-                this.tail[i] = this.tail[i+1];
-            }
-        }
-        this.tail[this.total-1] = createVector(this.x, this.y);
-
-        this.x = this.x + this.xspeed*scl;
-        this.y = this.y + this.yspeed*scl;
-
-        this.x = constrain(this.x, 0, width-scl);
-        this.y = constrain(this.y, 0, height-scl);
+  show() {
+    for (let i = 0; i < this.body.length; i++) {
+      fill(0);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1);
     }
-
-    this.show = function() {
-        fill(255);
-        for (var i = 0; i < this.tail.length; i++) {
-            rect(this.tail[i].x, this.tail[i].y, scl, scl);
-        }
-        fill(255);
-        rect(this.x, this.y, scl, scl);
-    }
+  }
 }
